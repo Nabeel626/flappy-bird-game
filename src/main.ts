47c3357bf2d4a3c30d5e42437fa244a7fbef20b8
Nabeel <nabeel92914@outlook.com>
2024-04-08 16:59:51 +0100

    Added soem animations and a loading screen. Created extra files so that play button and score button can be taken to another page

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+//bird width and height
+let birdwidth = 34;
+let birdheight = 24;
+let birdX = 360 / 8;
+let birdY = 640 / 2;
+
+let bird = {
+    x : birdX,
+    y : birdY,
+    width : birdwidth,
+    height: birdheight,
+}
+
+//WINDOW LOADER WHEN APP STARTS
+
+const showPage = () => { //This is a on show function where the window is loadded then shpow the page
+    const loadingBar = document.querySelector(".loadingbar");
+    const body = document.querySelector("#main");
+    const mainMenuPage = document.querySelector(".main-menu");
+
+    body?.setAttribute("style", "background-image: url('/images/flappy-bird-background.gif');");
+    loadingBar?.setAttribute("style", "display: none;");
+    mainMenuPage?.setAttribute("style", "display: grid;");
+}
+
+const onPageLoad = () => { //This is a on load page function when the window loadsit does a specific command
+    
+    const body = document.querySelector("#main");
+
+    body?.setAttribute("style", "width: 100vw; height: 100vh; background-size: cover; background-repeat:no-repeat; background-image: linear-gradient(to bottom, #99ccff, #8ad4f5, #8dd9e5, #9ddbd5, #b2dcc8);");
+    setTimeout(showPage, 4000); //this determines the loading time which is 4 seconds
+}
+
+window.onload = () => { //When the window starts then it will run the page load function
+    onPageLoad();
+}
