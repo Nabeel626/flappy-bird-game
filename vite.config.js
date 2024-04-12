816a272629bec56009592f8fb2ec536c637eb48f
Nabeel <nabeel92914@outlook.com>
2024-04-12 17:50:14 +0100

    saved file again

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         index: fileURLToPath(new URL("index.html", import.meta.url)),
         game: fileURLToPath(new URL("startGame.html", import.meta.url)),
         score: fileURLToPath(new URL("scoreboard.html", import.meta.url)),
+        style: fileURLToPath(new URL("./src/style.scss", import.meta.url)),
         bird1: fileURLToPath(new URL("./images/flappybirdv1.gif", import.meta.url)),
         bird2: fileURLToPath(new URL("./images/flappy-bird-background.gif", import.meta.url)),
         bird3: fileURLToPath(new URL("./images/loadingbar2.gif", import.meta.url)),
