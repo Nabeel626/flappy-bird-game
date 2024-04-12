f5e0dd8d009101f3e7e8e0ac8cb6a39c0cb245dd
Nabeel <nabeel92914@outlook.com>
2024-04-12 17:25:11 +0100

    changed file again

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
-export default {
-    base: "./",
-};
+import { defineConfig } from "vitest/config";
+import { fileURLToPath } from "url";
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        index: fileURLToPath(new URL("index.html", import.meta.url)),
+        game: fileURLToPath(new URL("startGame.html", import.meta.url)),
+        score: fileURLToPath(new URL("scoreboard.html", import.meta.url)),
+      },
+    },
+  },
+  base: "./",
+});
