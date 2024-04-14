97f1bab223f38c2f49b5496c94dec1b24ecabb10
Nabeel <nabeel92914@outlook.com>
2024-04-14 19:32:49 +0100

    changes

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const mainMenuPage = document.querySelector(".main-menu");
 //WINDOW LOADER WHEN APP STARTS
 const showPage = () => { //This is a on show function where the window is loadded then shpow the page
 
-    body?.setAttribute("style", `height: 100vh; margin: 0; box-sizing: border-box; background: url('src/images/flappybirdbackground.gif'); background-size: cover; background-repeat:no-repeat;`);
+    body?.setAttribute("style", `height: 100vh; margin: 0; box-sizing: border-box; background: url("./src/images/flappybirdbackground.gif"); background-size: cover; background-repeat:no-repeat;`);
     loadingBar?.setAttribute("style", "display: none;");
     mainMenuPage?.setAttribute("style", "display: grid;");
 
