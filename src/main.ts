cd6255ad8a01c6b0a0e128ff888901cd3293256b
Nabeel <nabeel92914@outlook.com>
2024-04-15 13:42:35 +0100

    changed preview styling and ensured images are working

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ const mainMenuPage = document.querySelector(".main-menu");
 
 //WINDOW LOADER WHEN APP STARTS
 const showPage = () => { //This is a on show function where the window is loadded then shpow the page
+    
+    const backgroundImgURL = new URL("/flappybirdbackground.gif", import.meta.url).href;
 
-    body?.setAttribute("style", "display: block; background-image: url('/src/images/flappybirdbackground.gif');");
     loadingBar?.setAttribute("style", "display: none;");
+    body?.setAttribute("style", `background-image: url(${backgroundImgURL});`);
     mainMenuPage?.setAttribute("style", "display: grid;");
-
-    getItems();
 }
 
-const onPageLoad = () => { //This is a on load page function when the window loadsit does a specific command
+const onPageLoad = () => { //This is a on load page function when the window loads it does a specific command
 
     body?.setAttribute("style", "width: 100vw; height: 100vh; background-size: cover; background-repeat:no-repeat; background-image: linear-gradient(to bottom, #99ccff, #8ad4f5, #8dd9e5, #9ddbd5, #b2dcc8);");
     setTimeout(showPage, 4000); //this determines the loading time which is 4 seconds
@@ -31,6 +31,8 @@ const getItems = () => {
 }
 
 window.onload = () => { //When the window starts then it will run the page load function
+    
     onPageLoad();
+    getItems();
     
 }
